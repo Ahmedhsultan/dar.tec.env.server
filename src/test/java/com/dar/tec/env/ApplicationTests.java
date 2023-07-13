5ef4d17aac74a3c12b2b0f808ec8d883e13de7e0
ahmedhsultan <ahmedhussamsultan@gmail.com>
2023-07-13 19:50:29 +0300

    initialize

---
+++ src/test/java/com/dar/tec/env/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dar.tec.env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
